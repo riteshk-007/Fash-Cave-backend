4a0be9cb01c4bb3a45ac74ec57f0b683b036af04
Ritesh <codeshorts007@gmail.com>
2023-08-26 18:50:13 +0530

    fix- error fixed

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * order controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::order.order');
+module.exports = createCoreController("api::order.order");
